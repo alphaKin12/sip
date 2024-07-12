1a25f60b96c05629c7043c3a809628a77e5ea022
alphaKin12 <ustaadaadi12@gmail.com>
2024-07-12 21:43:16 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -51,8 +51,8 @@ def calculate_sip_quarterly(monthly_investment, investment_period, expected_retu
         inflation_adjusted_rate = expected_return_rate - 5.0
         quarterly_rate = (inflation_adjusted_rate / 100) / 4
     
-    invested_amount = monthly_investment * 3 * quarters
-    future_value = monthly_investment * 3 * ((((1 + quarterly_rate) ** quarters) - 1) / quarterly_rate) * (1 + quarterly_rate)
+    invested_amount = monthly_investment *  quarters
+    future_value = (monthly_investment * 3) * ((((1 + quarterly_rate) ** quarters) - 1) / quarterly_rate) * (1 + quarterly_rate)
     
     return invested_amount, future_value
 
